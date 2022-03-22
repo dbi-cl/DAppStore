29f21d4060387cd8af18999132fcf07c5372385d
v-qizhao1 <v-qizhao1@microsoft.com>
2022-03-22 17:51:06 +0800

    web: add header and navigate

--- components/categories-card/CategoriesCard.pres.tsx
+++ components/categories-card/CategoriesCard.pres.tsx
@@ -4,17 +4,20 @@ import { FunctionComponent } from "react";
 import { ICategoriesCardProps } from "./CategoriesCard.props";
 
 const StyledButton = styled(MuiButton)(() => ({
-    "&:hover": {
-        backgroundColor: "#eee",
-    },
-    color: "#00000080",
-    "border-radius": "4px",
+  "&:hover": {
+    backgroundColor: "#eee",
+  },
+  color: "#00000080",
+  borderRadius: "4px",
 }));
 
-export const CategoriesCard: FunctionComponent<ICategoriesCardProps> = ({ text, onClick }: ICategoriesCardProps) => {
-    return (
-        <StyledButton variant="text" onClick={onClick}>
-            {text}
-        </StyledButton>
-    );
+export const CategoriesCard: FunctionComponent<ICategoriesCardProps> = ({
+  text,
+  onClick,
+}: ICategoriesCardProps) => {
+  return (
+    <StyledButton variant="text" onClick={onClick}>
+      {text}
+    </StyledButton>
+  );
 };
--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,29 +1,50 @@
-import { Categories, List } from "@/components"
-import { DApp } from "@/model/DApp"
-import type { NextPage } from "next"
+import { Categories, List } from "@/components";
+import { DApp } from "@/model/DApp";
+import { Button } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import type { NextPage } from "next";
+import styles from "./list.module.scss";
 
 interface IListPageProps {
-    list: DApp[];
+  list: DApp[];
 }
 
 const ListPage: NextPage<IListPageProps> = ({ list }) => {
-    return (
-        <>
-            <Categories />
-            <List list={list}></List>
-        </>
-    )
-}
+  return (
+    <>
+      <header className={styles.head}>
+        <div className={styles.routerTitle}>
+          <span>Home</span>
+          <span>Category</span>
+        </div>
+        <div className={styles.self}>
+          <Button
+            size="small"
+            variant="contained"
+            onClick={() => history.pushState("", "", "/submit")}
+          >
+            Submit Dapp
+          </Button>
+          <AccountCircleIcon sx={{ ml: 8, width: 70, height: 30 }} />
+        </div>
+      </header>
+      <Categories />
+      <div className={styles.list}>
+        <List list={list}></List>
+      </div>
+    </>
+  );
+};
 
 export const getStaticProps = async () => {
-    const res = await fetch("http://localhost:4000/list")
-    const list = await res.json()
+  const res = await fetch("http://localhost:4000/list");
+  const list = await res.json();
 
-    return {
-        props: {
-            list
-        }
-    }
-} 
+  return {
+    props: {
+      list: new Array(4).fill([...list]).flat(),
+    },
+  };
+};
 
-export default ListPage
+export default ListPage;
